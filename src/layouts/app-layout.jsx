c622bcef2fd87e9f62214f73a0328201128f0049
khaidprogramming <khalidmiya399@gmail.com>
2024-10-06 07:54:38 +0545

    fixed cors

---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,19 @@
+import Header from "@/components/header";
+import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <div className="grid-background"></div>
+      <main className="min-h-screen container">
+        <Header />
+        <Outlet />
+      </main>
+      <div className="p-10 text-center bg-gray-800 mt-10">
+      © 2024 Schedulrr. All rights reserved.
+      </div>
+    </div>
+  );
+};
+
+export default AppLayout;
